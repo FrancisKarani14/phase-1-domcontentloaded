a5f228f1f2cc93ec6cba79c8325612a2afe1907a
Francis <franciskarani15@gmail.com>
2025-06-20 09:34:28 +0300

    changes the content of the paragraph after loading DOM

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("the DOM content has loaded");
+    const paragraph =document.getElementById("text");
+    paragraph.textContent="This is really cool!"
+    
+    
+})
